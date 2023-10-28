a91d20cef8c6b679b8235c51dade4ef52c01f68d
Bandita Singh <115775569+banditasingh@users.noreply.github.com>
2023-10-28 22:18:55 +0530

    Add files via upload

---
+++ Anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    
+    def isAnagram(self,a,b):
+        list1 = list(a)
+        list2 = list(b)
+        
+        list1.sort()
+        list2.sort()
+        
+        string1 = ''.join(list1)
+        string2 = ''.join(list2)
+        
+        if string1 == string2:
+            return 1
+        else:
+            return 0
