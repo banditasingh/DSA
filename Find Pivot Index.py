ff50e0812b268b3c73dc1058711c1993a6c878c0
Bandita Singh <115775569+banditasingh@users.noreply.github.com>
2023-11-23 23:44:53 +0530

    Add files via upload

---
+++ Find Pivot Index.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+       
+        leftSum, rightSum = 0, sum(nums)
+        for idx, ele in enumerate(nums):
+            rightSum -= ele
+    
+            if leftSum == rightSum:
+                return idx    
+            leftSum += ele
+        return -1      
